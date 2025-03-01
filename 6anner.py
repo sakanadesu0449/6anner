16138aaeb942b603d1414f3873f0c437a12681cb
Sakana0449 <sakanadesu255378343@gmail.com>
2025-03-01 18:31:54 +0800

    v0.0.2: 实现了在线获取

--- 6anner.py
+++ 6anner.py
@@ -1,8 +1,69 @@
 import os
 import json
 import random
+import requests
+
+#from abc import ABC, abstractmethod
 
 PARENT = 'json'
+class BError(Exception):
+	pass
+
+class Robots():
+	@classmethod
+	def robot(cls, site: str, types: list[str]):
+
+		SITES = {
+			'hzmm': RobotHZZM
+		}
+		TYPES = ['ts', 'ss', 'sc']
+		if site not in SITES:
+			raise ValueError(f'unknown site: {site}')
+		for _t in types:
+			if _t not in TYPES:
+				raise ValueError(f'unknown type: {_t}')
+
+		return SITES[site](types)
+
+	def __init__(self, types: list[str]):
+		self.user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.0.0'
+
+		self.types = types
+		self.headers = {
+			'hzmm': {
+			'authority': 'hzzm.xusenlin.com',
+			'sec-ch-ua-platform': 'Windows',
+			'Origin': 'https://hz.xusenlin.com',
+			'User-Agent': self.user_agent
+			}
+		}
+
+class RobotHZZM(Robots):
+	def rand_page(self) -> str:
+		paginates = {
+			'ts': 5762,
+			'ss': 25425,
+			'sc': 2106
+		}
+		_type = random.choice(self.types)
+		rand_p = random.randint(1, paginates[_type])
+		return f'https://hzzm.xusenlin.com/v1/{_type}?pageNum={rand_p}&keyword='
+
+	def get_rand_verse(self) -> str:
+		url = self.rand_page()
+		#print(url)
+		page = requests.get(url, headers = self.headers['hzmm'])
+
+		#print(page.status_code)
+		_json = json.loads(page.text)
+		_paragraph_list = _json['data']['list']
+		rand_paragraph = random.choice(_paragraph_list)
+		author = rand_paragraph['author']
+		verse_list = rand_paragraph['paragraphs'].split('||')
+
+		return random.choice(verse_list), author
+
+
 
 def get_random_file(parent: str) -> str:
 
@@ -13,6 +74,19 @@ def get_random_file(parent: str) -> str:
 	return random.choice(files)
 
 if __name__ == '__main__':
+
+	r = Robots.robot('hzmm', ['ts', 'ss', 'sc'])
+	verses = r.get_rand_verse()
+	print(f'{verses[0]}\n              {verses[1]}')
+'''
+	except Exception as err:
+		print(err)
+'''
+
+
+
+
+'''
 	file_path = get_random_file(PARENT)
 	if 'song' in file_path:
 		dynasty = '宋'
@@ -26,3 +100,6 @@ if __name__ == '__main__':
 
 		print(rand_verse)
 		print(f'                {dynasty} - {rand_verse_poet}')
+'''
+
+
